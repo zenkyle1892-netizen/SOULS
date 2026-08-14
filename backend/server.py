7a3e7ec0b5372935e4b502f982bfdc91b8ef09dc
zenkyle1892-netizen <zenkyle1892@gmail.com>
2026-08-15 00:36:23 +0800

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -14,7 +14,7 @@ from datetime import datetime, timezone
 ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
-mongo_url = os.environ['MONGO_URL']
+mongo_url = os.environ.get['MONGO_URL']
 client = AsyncIOMotorClient(mongo_url)
 db = client[os.environ['DB_NAME']]
 
